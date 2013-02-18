3fd56bdd6e1e8b17e6f266617b50e101d10a957e
Hugo Cabral Tannús <hctannus@gmail.com>
2013-02-18 17:13:10 -0300

    Classe de exemplo para conexão com o banco.

---
+++ src/data/DataBaseInputExemple.java
@@ -0,0 +1,8 @@
+package data;
+
+public class DataBaseInputExemple {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DATABASE_URL = "jdbc:mysql://localhost/database";
+    static final String USERNAME = "user";
+    static final String PASSWORD = "password";
+}
